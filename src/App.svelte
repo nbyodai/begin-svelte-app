68d8752bf162a5deb1fa0a211897b8e35e1a2907
Chibuzor Obiora <nbyodai@gmail.com>
2020-02-15 10:19:31 +0100

    Remove onMount

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,12 @@
   <meta charset="UTF-8">
   <title>Chibuzor's Beninging</title>
   <script type="text/javascript" src="https://sdk.userbase.com/1/userbase.js"></script>
-	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </svelte:head>
 <script>
-  import { onMount } from 'svelte'
+  // initialise userbase
+  userbase.init({ appId: 'a19d663f-599e-4878-ba3b-c435c3e4b672' })
 	
-	onMount(async () => {
-    userbase.init({ appId: 'a19d663f-599e-4878-ba3b-c435c3e4b672' })
-  })
-  
   let username;
   let password;
   let error;
@@ -23,11 +20,11 @@
 
 </script>
 
-<div id="auth-view" class="container flex flex-col items-center bg-gray-100 h-screen">
+<div id="auth-view" class="container mx-auto flex flex-col items-center bg-gray-100 h-screen">
 	<h1 class="uppercase m-2 font-semibold text-3xl text-blue-300">Create an account</h1>
-	<form id="signup-form" class="flex flex-col items-center bg-gray-200 mt-8 w-1/2">
-		<input id="signup-username" class="border-dashed border-2 border-blue-200 border-2 rounded-lg w-1/2 mt-4 p-2 focus:outline-none" type="text" required placeholder="Username" bind:value={username}>
-		<input id="signup-password" class="border-dashed border-2 border-blue-200 border-2 rounded-lg w-1/2 mt-4 p-2 focus:outline-none" type="password" required placeholder="Password" bind:value={password}>
+	<form id="signup-form" class="flex flex-col items-center mt-8 w-1/2 h-screen">
+		<input id="signup-username" class="border-dashed border-2 border-blue-200 border-2 rounded-lg w-2/3 mt-4 p-2 focus:outline-none" type="text" required placeholder="Username" bind:value={username}>
+		<input id="signup-password" class="border-dashed border-2 border-blue-200 border-2 rounded-lg w-2/3 mt-4 p-2 focus:outline-none" type="password" required placeholder="Password" bind:value={password}>
 		<button class="bg-blue-200 hover:bg-blue-300 text-white font-bold py-2 px-4 border border-blue-300 rounded my-4 w-2/3" on:click={handleCreateAccount}> Create an account</button>
 	</form>
 	{#if error}
